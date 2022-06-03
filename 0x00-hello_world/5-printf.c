7882b1739280032096486481d333c49cd09be3be
Affluent11 <jogreatj@gmail.com>
2022-06-03 06:08:53 -0700

    re

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 
 /**
  * main - Entry point
- * 
+ * @0 - The return
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
 }
