e7d3f2d2190ba24e0caf9bde016a1e0f31f7d77d
Affluent11 <jogreatj@gmail.com>
2022-06-10 06:49:50 -0700

    good

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,9 +13,9 @@ int main(void)
 	i = 0;
 	/**
 	* while (i < 10)
-	* { 
-	* 	Infinite loop - No increase of variable
-	* 	putchar(i);
+	* {
+	*	Infinite loop - No increase of variable
+	*	putchar(i);
 	* }
 	*/
 printf("Infinite loop avoided! \\o/\n");
