bc3d8a2c70f5acf2a8da6c4099e59717365f07df
Affluent11 <jogreatj@gmail.com>
2022-06-14 12:28:17 -0700

    better

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,10 +2,9 @@
 
 
 /**
- * main - Entry point
- * _strcpy - Copies a string pointed
- * A pointer to an char that will be updated
- * A source string to copy.
+ * *_strcpy - Copies a string pointed
+ * @dest: A pointer to an char that will be updated
+ * @src: A source string to copy.
  * Return: A pointer to the dest. string @dest.
  */
 char *_strcpy(char *dest, char *src)
