5a2423a710f8de50c2d136bd2bd4b6c9f3ec37bd
Affluent11 <jogreatj@gmail.com>
2022-06-24 12:29:24 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,7 +9,7 @@
  * Return: If one of the numbers contains symbols that are non-digits - 1.
  * Otherwise - 0.
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]);
 {
 	int num, digit, sum = 0;
 
